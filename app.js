adb9154c56543e638fa91a5c344e1f1de7e5d7c6
YuZheLin1987 <billlinxx@gmail.com>
2022-11-29 22:39:47 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 // 載入
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const port = 3000
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // 設定路由
 app.get('/', (req, res) => {
